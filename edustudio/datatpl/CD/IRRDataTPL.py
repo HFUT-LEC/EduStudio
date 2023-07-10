fdfb8e41e8030fbc53bc8a0ce7d24bc2e2c10c32
tzt-star <74764076+tzt-star@users.noreply.github.com>
2023-07-10 19:33:41 +0800

    Update IRRDataTPL.py
    Fix IRR bugs partly

--- edustudio/datatpl/CD/IRRDataTPL.py
+++ edustudio/datatpl/CD/IRRDataTPL.py
@@ -19,7 +19,7 @@ class IRRDataTPL(BaseProxyDataTPL):
     default_cfg = {
         'backbone_datatpl_cls': 'CDInterDataTPL',
         'num_observed': 10,
-        'num_unobserved': 10
+        'num_unobserved': 0
     }
 
     def sample(self):
@@ -29,70 +29,80 @@ class IRRDataTPL(BaseProxyDataTPL):
         stu_id = self.dict_main['stu_id'].numpy()
         exer_id = self.dict_main['exer_id'].numpy()
         label = self.dict_main['label'].numpy()
-        do_right_stu = {}
-        do_wrong_stu = {}
+        students_with_correct_answers = {}
+        students_with_wrong_answers = {}
+
         for i in range(stu_id.shape[0]):
             if label[i] == 0:
-                if exer_id[i] in do_wrong_stu:
-                    do_wrong_stu[exer_id[i]].append(stu_id[i])
-                else:
-                    do_wrong_stu[exer_id[i]] = [stu_id[i]]
+                students_with_wrong_answers.setdefault(exer_id[i], []).append(stu_id[i])
             else:
-                if exer_id[i] in do_right_stu:
-                    do_right_stu[exer_id[i]].append(stu_id[i])
-                else:
-                    do_right_stu[exer_id[i]] = [stu_id[i]]
+                students_with_correct_answers.setdefault(exer_id[i], []).append(stu_id[i])
+
         self.dict_main['pair_exer'] = []
         self.dict_main['pair_pos_stu'] = []
         self.dict_main['pair_neg_stu'] = []
+
         for i in range(stu_id.shape[0]):
             self.pair_exer = exer_id[i]
+
             if label[i] == 0:
                 self.pair_neg_stu = stu_id[i]
-                do_right_stu_exer = []
-                if self.pair_exer in do_right_stu:
-                    do_right_stu_exer = do_right_stu[self.pair_exer]
-                    if len(do_right_stu[self.pair_exer])<self.num_observed:
-                        for j in range(len(do_right_stu[self.pair_exer])):
-                            self.dict_main['pair_exer'].append(self.pair_exer)
-                            self.dict_main['pair_pos_stu'].append(do_right_stu[self.pair_exer][j])
-                            self.dict_main['pair_neg_stu'].append(self.pair_neg_stu)
-                    else:
-                        pos_stu_list = random.sample(do_right_stu[self.pair_exer], k=self.num_observed)
-                        for j in range(self.num_observed):
-                            self.dict_main['pair_exer'].append(self.pair_exer)
-                            self.dict_main['pair_pos_stu'].append(pos_stu_list[j])
-                            self.dict_main['pair_neg_stu'].append(self.pair_neg_stu)
-                for j in range(self.num_unobserved):
-                    self.pair_pos_stu = random.randint(0, self.stu_count - 1)
-                    while self.pair_pos_stu in do_right_stu_exer:
-                         self.pair_pos_stu = random.randint(0, self.stu_count - 1)
-                    self.dict_main['pair_exer'].append(self.pair_exer)
-                    self.dict_main['pair_pos_stu'].append(numpy.int64(self.pair_pos_stu))
-                    self.dict_main['pair_neg_stu'].append(self.pair_neg_stu)
+                do_right_stu_exer = students_with_correct_answers.get(self.pair_exer, [])
+                do_right_stu_exer_length = len(do_right_stu_exer)
+
+                if do_right_stu_exer_length < self.num_observed:
+                    for j in range(do_right_stu_exer_length):
+                        self.add_pair(self.pair_exer, do_right_stu_exer[j], self.pair_neg_stu)
+                else:
+                    pos_stu_list = random.sample(do_right_stu_exer, k=self.num_observed)
+                    for j in range(self.num_observed):
+                        self.add_pair(self.pair_exer, pos_stu_list[j], self.pair_neg_stu)
+
+                unobserved = []
+                do_wrong_stu_exer = students_with_wrong_answers.get(self.pair_exer, [])
+
+                while len(unobserved) < self.num_unobserved and len(unobserved) < do_right_stu_exer_length:
+                    self.pair_pos_stu = numpy.int64(random.randint(0, self.stu_count - 1))
+
+                    while (self.pair_pos_stu in do_right_stu_exer) or (self.pair_pos_stu in do_wrong_stu_exer) or (self.pair_pos_stu in unobserved):
+                        self.pair_pos_stu = numpy.int64(random.randint(0, self.stu_count - 1))
+
+                    unobserved.append(self.pair_pos_stu)
+
+                for j in range(len(unobserved)):
+                    self.add_pair(self.pair_exer, unobserved[j], self.pair_neg_stu)
             else:
                 self.pair_pos_stu = stu_id[i]
-                do_wrong_stu_exer = []
-                if self.pair_exer in do_wrong_stu:
-                    do_wrong_stu_exer = do_wrong_stu[self.pair_exer]
-                    if len(do_wrong_stu[self.pair_exer])<self.num_observed:
-                        for j in range(len(do_wrong_stu[self.pair_exer])):
-                            self.dict_main['pair_exer'].append(self.pair_exer)
-                            self.dict_main['pair_pos_stu'].append(self.pair_pos_stu)
-                            self.dict_main['pair_neg_stu'].append(do_wrong_stu[self.pair_exer][j])
-                    else:
-                        neg_stu_list = random.sample(do_wrong_stu[self.pair_exer], k=self.num_observed)
-                        for j in range(self.num_observed):
-                            self.dict_main['pair_exer'].append(self.pair_exer)
-                            self.dict_main['pair_pos_stu'].append(self.pair_pos_stu)
-                            self.dict_main['pair_neg_stu'].append(neg_stu_list[j])
-                for j in range(self.num_unobserved):
-                    self.pair_neg_stu = random.randint(0, self.stu_count - 1)
-                    while self.pair_neg_stu in do_wrong_stu_exer:
-                         self.pair_neg_stu = random.randint(0, self.stu_count - 1)
-                    self.dict_main['pair_exer'].append(self.pair_exer)
-                    self.dict_main['pair_pos_stu'].append(self.pair_pos_stu)
-                    self.dict_main['pair_neg_stu'].append(numpy.int64(self.pair_neg_stu))
+                do_wrong_stu_exer = students_with_wrong_answers.get(self.pair_exer, [])
+                do_wrong_stu_exer_length = len(do_wrong_stu_exer)
+
+                if do_wrong_stu_exer_length < self.num_observed:
+                    for j in range(do_wrong_stu_exer_length):
+                        self.add_pair(self.pair_exer, self.pair_pos_stu, do_wrong_stu_exer[j])
+                else:
+                    neg_stu_list = random.sample(do_wrong_stu_exer, k=self.num_observed)
+                    for j in range(self.num_observed):
+                        self.add_pair(self.pair_exer, self.pair_pos_stu, neg_stu_list[j])
+
+                unobserved = []
+                do_right_stu_exer = students_with_correct_answers.get(self.pair_exer, [])
+
+                while len(unobserved) < self.num_unobserved and len(unobserved) < do_wrong_stu_exer_length:
+                    self.pair_neg_stu = numpy.int64(random.randint(0, self.stu_count - 1))
+
+                    while (self.pair_neg_stu in do_right_stu_exer) or (self.pair_neg_stu in do_wrong_stu_exer) or (self.pair_neg_stu in unobserved):
+                        self.pair_neg_stu = numpy.int64(random.randint(0, self.stu_count - 1))
+
+                    unobserved.append(self.pair_neg_stu)
+
+                for j in range(len(unobserved)):
+                    self.add_pair(self.pair_exer, self.pair_pos_stu, unobserved[j])
+
+    def add_pair(self, exer_id, pos_stu, neg_stu):
+        self.dict_main['pair_exer'].append(exer_id)
+        self.dict_main['pair_pos_stu'].append(pos_stu)
+        self.dict_main['pair_neg_stu'].append(neg_stu)
+
 
     def build_dataloaders(self):
         batch_size = self.traintpl_cfg['batch_size']
